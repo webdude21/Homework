d69912c5fe73ddb0c87c3817f7e868114907e5ec
Димо Петров <dimo.petrof@gmail.com>
2014-02-12 13:33:47 +0200

    Small fixes

--- OOP/OOP-Principles-Part1/Humans/Human.cs
+++ OOP/OOP-Principles-Part1/Humans/Human.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Humans
+﻿namespace Humans
 {
     public abstract class Human
     {
         public string FirstName { get; set; }
-
         public string LastName { get; set; }
 
         public Human(string firstName, string lastName)
