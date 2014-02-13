669431f85eddaa240b990e5043514117fd7f4169
Димо Петров <dimo.petrof@gmail.com>
2014-02-13 18:03:07 +0200

    Finished the OOP Principles Part2 homework

---
+++ OOP/OOP-Principles-Part2/GenericRangeException/GenericRangeException.cs
@@ -0,0 +1,54 @@
+﻿
+/* Define a class InvalidRangeException<T> that holds information about an error condition 
+ * related to invalid range. It should hold error message and a range definition [start … end].
+ * Write a sample application that demonstrates the InvalidRangeException<int> and 
+ * InvalidRangeException<DateTime> by entering numbers in the range [1..100] and dates in 
+ * the range [1.1.1980 … 31.12.2013]. */
+using System;
+
+namespace GenericRangeException
+{
+    class GenericRangeException
+    {
+        static void Main()
+        {
+            {
+                try
+                {
+                    int rangeStart = 10;
+                    int rangeEnd = 155;
+
+                    int variableToCheck = 200;
+
+                    if (!(rangeStart < variableToCheck && variableToCheck < rangeEnd))
+                        throw new InvalidRangeException<int>(rangeStart, rangeEnd);
+                }
+
+                catch (InvalidRangeException<int> iRError)
+                {
+                    Console.WriteLine(iRError.Message);
+                    Console.WriteLine("Start: {0}; End {1};", iRError.Start, iRError.End);
+                }
+            }
+            Console.WriteLine("Date check:");
+            {
+                try
+                {
+                    DateTime startDate = new DateTime(1984, 08, 17);
+                    DateTime endDate = new DateTime(2215, 12, 31);
+
+                    DateTime dateToCheck = DateTime.MinValue;
+
+                    if (!(startDate < dateToCheck && dateToCheck < endDate))
+                        throw new InvalidRangeException<DateTime>(startDate, endDate);
+                }
+
+                catch (InvalidRangeException<DateTime> iRError)
+                {
+                    Console.WriteLine(iRError.Message);
+                    Console.WriteLine("Start: {0}; End {1};", iRError.Start, iRError.End);
+                }
+            }
+        }
+    }
+}
---
+++ OOP/OOP-Principles-Part2/GenericRangeException/InvalidRangeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class InvalidRangeException<T> : Exception
+{
+    public T Start { get; protected set; }
+    public T End { get; protected set; }
+
+    public InvalidRangeException(T start, T end, Exception innerException = null)
+        : base("Out of range!", innerException)
+    {
+        this.Start = start;
+        this.End = end;
+    }
+}
