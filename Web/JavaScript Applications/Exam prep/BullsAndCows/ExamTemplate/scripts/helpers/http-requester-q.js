3f799282ac225dfb6a2d353d2bc6031c8ce4dcc2
Димо Петров <webdude@webdude.eu>
2014-07-30 11:53:16 +0300

    с
    с

---
+++ Web/JavaScript Applications/Exam prep/BullsAndCows/ExamTemplate/scripts/helpers/http-requester-q.js
@@ -0,0 +1,36 @@
+define(['jquery', 'q'], function($, Q) {
+    return (function () {
+        var makeHttpRequest = function (url, type, data) {
+            var deferred = Q.defer();
+
+            $.ajax({
+                url: url,
+                type: type,
+                data: data ? JSON.stringify(data) : "",
+                contentType: "application/json",
+                timeout: 5000,
+                success: function (resultData) {
+                    deferred.resolve(resultData);
+                },
+                error: function (errorData) {
+                    deferred.reject(errorData);
+                }
+            });
+
+            return deferred.promise;
+        };
+
+        var getJSON = function (url) {
+            return makeHttpRequest(url, "GET");
+        };
+
+        var postJSON = function (url, data) {
+            return makeHttpRequest(url, "POST", data);
+        };
+
+        return {
+            getJSON: getJSON,
+            postJSON: postJSON
+        }
+    }());
+});
