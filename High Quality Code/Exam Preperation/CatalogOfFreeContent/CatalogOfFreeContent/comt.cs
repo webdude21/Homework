8e5b6fbe86ff24aed9fe21da071b3f73188d826c
Димо Петров <webdude@webdude.eu>
2014-08-05 16:54:32 +0300

    s
    s

--- High Quality Code/Exam Preperation/CatalogOfFreeContent/CatalogOfFreeContent/comt.cs
+++ High Quality Code/Exam Preperation/CatalogOfFreeContent/CatalogOfFreeContent/comt.cs
@@ -1,31 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Problem04_Free_Content
+﻿namespace CatalogOfFreeContent
 {
     public enum comt
     {
-        AddBook,
-        AddMovie, AddSong,
-        AddApplication,
-        Update, Find,
-    }
-    public enum ct
-    {
-        Kniga,
-        Film,
-        Muzika,
-        Prilozenie,
-    }
+        AddBook, 
 
-    public enum acpi
-    {
-        Title = 0,        Author,
-        Size,
-        Url,
-    }
+        AddMovie, 
 
+        AddSong, 
+
+        AddApplication, 
+
+        Update, 
+
+        Find, 
+    }
 }
