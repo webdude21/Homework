35df8bbbeffad5d59aba6196517a4c7651820478
Димо Петров <webdude@webdude.eu>
2014-09-06 12:57:45 +0300

    Exam prep

---
+++ Databases/Exam Preparation/ToyStore/Bookstore.Data/BookstoreDbContext.cs
@@ -0,0 +1,31 @@
+﻿namespace Bookstore.Model
+{
+    using System.Data.Entity;
+
+    using Bookstore.Data.Migrations;
+
+    public class BookstoreDbContext : DbContext
+    {
+        public BookstoreDbContext()
+            : base("BookstoreConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BookstoreDbContext, Configuration>());
+        }
+
+        public IDbSet<Book> Books { get; set; }
+
+        public IDbSet<Review> Reviews { get; set; }
+
+        public IDbSet<Author> Authors { get; set; }
+
+        public new IDbSet<T> Set<T>() where T : class
+        {
+            return base.Set<T>();
+        }
+
+        public new void SaveChanges()
+        {
+            base.SaveChanges();
+        }
+    }
+}
