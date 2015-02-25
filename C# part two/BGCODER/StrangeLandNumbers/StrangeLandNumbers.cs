3797b0bd0926504a89b7b6d48f55dc1eeda11a5a
Димо Петров <webdude@webdude.eu>
2015-02-25 23:21:09 +0200

    BGcoder

---
+++ C# part two/BGCODER/StrangeLandNumbers/StrangeLandNumbers.cs
@@ -0,0 +1,29 @@
+﻿namespace StrangeLandNumbers
+{
+    using System;
+
+    internal class StrangeLandNumbers
+    {
+        private static void Main()
+        {
+            var numbers = new[] { "f", "bIN", "oBJEC", "mNTRAVL", "lPVKNQ", "pNWE", "hT" };
+            var result = Console.ReadLine();
+            long output = 0;
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                result = result.Replace(numbers[i], i.ToString());
+            }
+
+            var power = 0;
+
+            for (var i = result.Length - 1; i >= 0; i--)
+            {
+                output += (long)((result[i] - '0') * Math.Pow(7d, power));
+                power += 1;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ C# part two/BGCODER/TwoGirlsOnePath/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace TwoGirlsOnePath
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var field = Console.ReadLine().Split().Select(BigInteger.Parse).ToArray();
+            var molly = new Girl(0, "Molly");
+            var dolly = new Girl(field.Length - 1, "Dolly");
+            const bool gameHasEnded = false;
+
+            while (!gameHasEnded)
+            {
+                if (molly.Position == dolly.Position)
+                {
+                    var flowers = field[molly.Position];
+                    molly.Flowers += flowers / 2;
+                    dolly.Flowers += flowers / 2;
+
+                    field[molly.Position] = flowers % 2;
+                }
+            }
+        }
+
+        public class Girl
+        {
+            public Girl(int position, string name)
+            {
+                this.Position = position;
+                this.Name = name;
+                this.Flowers = 0;
+            }
+
+            public int Position { get; set; }
+
+            public string Name { get; set; }
+
+            public BigInteger Flowers { get; set; }
+        }
+    }
+}
