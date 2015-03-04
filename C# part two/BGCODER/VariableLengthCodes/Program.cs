72c6d8d34e62de40e60250085c56deef3db83416
Димо Петров <webdude@webdude.eu>
2015-03-04 23:54:52 +0200

    Exam prep

---
+++ C# part two/BGCODER/VariableLengthCodes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace VariableLengthCodes
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var encodedText = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var numbersOfLinesOfTheTable = int.Parse(Console.ReadLine());
+        }
+    }
+}
