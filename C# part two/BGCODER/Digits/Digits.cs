971d5efa3c0c55b048f6ba707bffe8108e4bebb2
Димо Петров <webdude@webdude.eu>
2015-03-05 00:00:44 +0200

    Some files weren't included

---
+++ C# part two/BGCODER/Digits/Digits.cs
@@ -0,0 +1,252 @@
+﻿namespace Digits
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Digits
+    {
+        private static int[,] matrix;
+
+        private static List<bool[,]> patterns;
+
+        private static void Main()
+        {
+            var n = ReadInput();
+            LoadPatterns();
+            var sum = Solve(n);
+            Console.WriteLine(sum);
+        }
+
+        private static int Solve(int n)
+        {
+            var sum = 0;
+
+            for (var row = 0; row <= n - 5; row++)
+            {
+                for (var col = 0; col <= n - 3; col++)
+                {
+                    for (var index = 0; index < patterns.Count; index++)
+                    {
+                        if (CheckForPattern(patterns[index], index + 1, row, col))
+                        {
+                            sum += index + 1;
+                        }
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private static bool CheckForPattern(bool[,] pattern, int digit, int row, int col)
+        {
+            var rowMax = pattern.GetLength(0);
+            var colMax = pattern.GetLength(1);
+
+            for (var i = 0; i < rowMax; i++)
+            {
+                for (var j = 0; j < colMax; j++)
+                {
+                    if (pattern[i, j])
+                    {
+                        if (matrix[i + row, j + col] != digit)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private static void LoadPatterns()
+        {
+            patterns = new List<bool[,]>();
+
+            // ONE
+            patterns.Add(
+                new[,]
+                    {
+                        {
+                           false, false, true 
+                        }, {
+                              false, true, true 
+                           }, {
+                                 true, false, true 
+                              }, {
+                                    false, false, true 
+                                 }, 
+                        {
+                           false, false, true 
+                        }
+                    });
+
+            // TWO
+            patterns.Add(
+                new[,]
+                    {
+                        {
+                           false, true, false 
+                        }, {
+                              true, false, true 
+                           }, {
+                                 false, false, true 
+                              }, {
+                                    false, true, false 
+                                 }, 
+                        {
+                           true, true, true 
+                        }
+                    });
+
+            // Three
+            patterns.Add(
+                new[,]
+                    {
+                        {
+                           true, true, true 
+                        }, {
+                              false, false, true 
+                           }, {
+                                 false, true, true 
+                              }, {
+                                    false, false, true 
+                                 }, 
+                        {
+                           true, true, true 
+                        }
+                    });
+
+            // Four
+            patterns.Add(
+                new[,]
+                    {
+                        {
+                           true, false, true 
+                        }, {
+                              true, false, true 
+                           }, {
+                                 true, true, true 
+                              }, {
+                                    false, false, true 
+                                 }, 
+                        {
+                           false, false, true 
+                        }
+                    });
+
+            // Five
+            patterns.Add(
+                new[,]
+                    {
+                        {
+                           true, true, true 
+                        }, {
+                              true, false, false 
+                           }, {
+                                 true, true, true 
+                              }, {
+                                    false, false, true 
+                                 }, 
+                        {
+                           true, true, true 
+                        }
+                    });
+
+            // Six
+            patterns.Add(
+                new[,]
+                    {
+                        {
+                           true, true, true 
+                        }, {
+                              true, false, false 
+                           }, {
+                                 true, true, true 
+                              }, {
+                                    true, false, true 
+                                 }, 
+                        {
+                           true, true, true 
+                        }
+                    });
+
+            // Seven
+            patterns.Add(
+                new[,]
+                    {
+                        {
+                           true, true, true 
+                        }, {
+                              false, false, true 
+                           }, {
+                                 false, true, false 
+                              }, {
+                                    false, true, false 
+                                 }, 
+                        {
+                           false, true, false 
+                        }
+                    });
+
+            // Eight
+            patterns.Add(
+                new[,]
+                    {
+                        {
+                           true, true, true 
+                        }, {
+                              true, false, true 
+                           }, {
+                                 false, true, false 
+                              }, {
+                                    true, false, true 
+                                 }, 
+                        {
+                           true, true, true 
+                        }
+                    });
+
+            // Nine
+            patterns.Add(
+                new[,]
+                    {
+                        {
+                           true, true, true 
+                        }, {
+                              true, false, true 
+                           }, {
+                                 false, true, true 
+                              }, {
+                                    false, false, true 
+                                 }, 
+                        {
+                           true, true, true 
+                        }
+                    });
+        }
+
+        private static int ReadInput()
+        {
+            var n = int.Parse(Console.ReadLine());
+            matrix = new int[n, n];
+
+            for (var row = 0; row < n; row++)
+            {
+                var input = Console.ReadLine();
+                var col = 0;
+                for (var i = 0; i < input.Length; i++)
+                {
+                    if (input[i] != ' ')
+                    {
+                        matrix[row, col] = input[i] - '0';
+                        col++;
+                    }
+                }
+            }
+
+            return n;
+        }
+    }
+}
---
+++ C# part two/BGCODER/RelevanceIndex/RelevanceIndex.cs
@@ -0,0 +1,65 @@
+﻿namespace RelevanceIndex
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class RelevanceIndex
+    {
+        private static void Main()
+        {
+            var searchWord = Console.ReadLine();
+            var punctuation = new[] { ",", ".", "(", ")", ";", "-", "!", "?", " "};
+            var linesOfInput = int.Parse(Console.ReadLine());
+            var relevenceIndexList = new SortedSet<Paragraph>();
+
+            for (var i = 0; i < linesOfInput; i++)
+            {
+                ReadRelevenceIndex(searchWord, punctuation, relevenceIndexList);
+            }
+
+            foreach (var paragraph in relevenceIndexList)
+            {
+                Console.WriteLine(paragraph);
+            }
+        }
+
+        private static void ReadRelevenceIndex(string searchWord, string[] punctuation, ICollection<Paragraph> relevenceIndexList)
+        {
+            var input = Console.ReadLine().Split(punctuation, StringSplitOptions.RemoveEmptyEntries);
+            var relevenceIndex = 0;
+
+            for (var currentWord = 0; currentWord < input.Length; currentWord++)
+            {
+                if (input[currentWord].ToUpperInvariant() == searchWord.ToUpperInvariant())
+                {
+                    input[currentWord] = input[currentWord].ToUpperInvariant();
+                    relevenceIndex++;
+                }
+            }
+            relevenceIndexList.Add(new Paragraph(relevenceIndex, input));
+        }
+
+        public class Paragraph : IComparable<Paragraph>
+        {
+            public int RelevenceIndex { get; set; }
+
+            public string[] WordsList { get; set; }
+
+            public Paragraph(int relevenceIndex, string[] wordsList)
+            {
+                this.RelevenceIndex = relevenceIndex;
+                this.WordsList = wordsList;
+            }
+
+            public int CompareTo(Paragraph other)
+            {
+                return -this.RelevenceIndex.CompareTo(other.RelevenceIndex);
+            }
+
+            public override string ToString()
+            {
+                return string.Join(" ", this.WordsList);
+            }
+        }
+    }
+}
