27b252393068b037beba45f05059709112452794
Димо <webdude@webdude.eu>
2016-06-04 17:13:15 +0300

    style improvment

--- C# part two/BGCODER/PenguinAirlines/PenguinAirlines.cs
+++ C# part two/BGCODER/PenguinAirlines/PenguinAirlines.cs
@@ -5,13 +5,17 @@
 
     internal class PenguinAirlines
     {
+        private const string DirectFlight = "There is a direct flight.";
+
+        private const string NoFlightsAvailable = "No flights available.";
+
+        private const string NoMoreQueries = "Have a break";
+
+        private const string NoFlights = "None";
+
         private static int islands;
 
         private static bool[,] adjacencyMatrix;
-
-        private const string DirectFlight = "There is a direct flight.";
-
-        private const string NoFlight = "No flights available.";
 
         private static void Main()
         {
@@ -23,7 +27,7 @@
         {
             var queryInput = Console.ReadLine();
 
-            while (queryInput != "Have a break")
+            while (queryInput != NoMoreQueries)
             {
                 var parsedInput = queryInput.Split();
                 Console.WriteLine(CheckForFligths(int.Parse(parsedInput[0]), int.Parse(parsedInput[1])));
@@ -33,12 +37,7 @@
 
         private static string CheckForFligths(int from, int to)
         {
-            if (adjacencyMatrix[from, to])
-            {
-                return DirectFlight;
-            }
-
-            return NoFlight;
+            return adjacencyMatrix[from, to] ? DirectFlight : NoFlightsAvailable;
         }
 
         private static void ReadInput()
@@ -48,7 +47,7 @@
 
             for (var fligthFrom = 0; fligthFrom < islands; fligthFrom++)
             {
-                foreach (var fligthTo in Console.ReadLine().Split().Where(f => f != "None").Select(int.Parse))
+                foreach (var fligthTo in Console.ReadLine().Split().Where(f => f != NoFlights).Select(int.Parse))
                 {
                     adjacencyMatrix[fligthFrom, fligthTo] = true;
                     adjacencyMatrix[fligthTo, fligthFrom] = true;
