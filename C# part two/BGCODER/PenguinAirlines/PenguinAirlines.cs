005b20af9c37a5f566611ca1573b2db9ff070316
Димо <webdude@webdude.eu>
2016-06-05 10:45:07 +0300

    used disjoint set this thime still no luck to get 100/100

--- C# part two/BGCODER/PenguinAirlines/PenguinAirlines.cs
+++ C# part two/BGCODER/PenguinAirlines/PenguinAirlines.cs
@@ -2,12 +2,15 @@
 {
     using System;
     using System.Linq;
+    using System.Threading;
 
     internal class PenguinAirlines
     {
         private const string DirectFlight = "There is a direct flight.";
 
         private const string NoFlightsAvailable = "No flights available.";
+
+        private const string IndirectFlights = "There are flights, unfortunately they are not direct, grandma :(";
 
         private const string NoMoreQueries = "Have a break";
 
@@ -17,8 +20,11 @@
 
         private static bool[,] adjacencyMatrix;
 
+        private static int[] disjointSet;
+
         private static void Main()
         {
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
             ReadInput();
             RespondToQueries();
         }
@@ -35,24 +41,65 @@
             }
         }
 
+        private static int Find(int i)
+        {
+            if (disjointSet[i] < 0)
+            {
+                return i;
+            }
+
+            disjointSet[i] = Find(disjointSet[i]);
+
+            return disjointSet[i];
+        }
+
+        private static bool Connected(int from, int y)
+        {
+            return Find(from) == Find(y);
+        }
+
         private static string CheckForFligths(int from, int to)
         {
-            return adjacencyMatrix[from, to] ? DirectFlight : NoFlightsAvailable;
+            if (adjacencyMatrix[from, to])
+            {
+                return DirectFlight;
+            }
+
+            if (Connected(from, to))
+            {
+                return IndirectFlights;
+            }
+
+            return NoFlightsAvailable;
         }
 
         private static void ReadInput()
         {
             islands = int.Parse(Console.ReadLine());
+            disjointSet = Enumerable.Repeat(-1, islands).ToArray();
             adjacencyMatrix = new bool[islands, islands];
-
             for (var fligthFrom = 0; fligthFrom < islands; fligthFrom++)
             {
                 foreach (var fligthTo in Console.ReadLine().Split().Where(f => f != NoFlights).Select(int.Parse))
                 {
                     adjacencyMatrix[fligthFrom, fligthTo] = true;
                     adjacencyMatrix[fligthTo, fligthFrom] = true;
+                    Union(fligthFrom, fligthTo);
                 }
             }
+        }
+
+        private static void Union(int from, int to)
+        {
+            from = Find(from);
+            to = Find(to);
+
+            if (from == to)
+            {
+                return;
+            }
+
+            disjointSet[from] = to;
         }
     }
 }
